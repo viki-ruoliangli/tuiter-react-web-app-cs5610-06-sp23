813247716eff4be4f30a30b82ecf09a44e8ffde4
viki-ruoliangli <liruolianglrllrl@gmail.com>
2023-04-02 00:46:53 -0700

    a8 commit 3

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -22,6 +22,8 @@ export const deleteTuit = async (tid) => {
 
 export const updateTuit = async (tuit) => {
   const response = await axios.put(`${TUITS_API}/${tuit._id}`, tuit);
-  return tuit;
+  // return tuit;
+  return response.data;
 }
+
 //
